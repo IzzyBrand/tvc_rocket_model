afb556ce1cc60456a268c60628d1f8d6150a6a9d
Izzy Brand <isaiah_brand@brown.edu>
2019-05-02 12:49:03 -0400

    switched animation to use arrow and real timesteps for animating

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.patches as mpatches
 
 sin = np.sin
 cos = np.cos
@@ -16,6 +17,10 @@ I = 1.        # the rotational inertia of the rocket
 l = -0.5        # the distance from the CG of the rocket to the center of aerodynamic pressure
 g = 9.8     # acceleration due to gravity
 
+
+dt = 0.001
+duration = 10
+framerate = 20
 
 # a function that returns a thrust angle based on the rocket's state
 def control(u):
@@ -42,34 +47,39 @@ def ddt(u, t):
 def animate(x, y, theta):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_xlim(np.min(x)-10, np.max(x)+10)
-    ax.set_ylim(np.min(y)-10, np.max(y)+10)
-    scat, = ax.plot(x[0], y[0], ls=" ", marker=(3, 0, theta[0]), markersize=20)
 
     def update(t):
-        scat.set_data(x[15*t], y[15*t])
-        scat.set_marker((3, 0, np.degrees(theta[10*t])))
-        return scat,
+        t = int(t/(framerate*dt))
 
-    ani = FuncAnimation(fig, update, frames=len((x-1)//10), interval=1)
+        ax.clear()
+
+        plt.xlim(np.min(x)-10, np.max(x)+10)
+        plt.ylim(np.min(y)-10, np.max(y)+10)
+        plt.gca().set_aspect('equal', adjustable='box')
+
+        size = 10
+        arrow = mpatches.Arrow(x[t], y[t],
+                               -size*np.sin(theta[t]), size*np.cos(theta[t]), width=size)
+        ax.add_patch(arrow)
+
+    ani = FuncAnimation(fig, update, frames=int(len(x)*(framerate*dt)-1), interval=1000./framerate)
     plt.show()
 
 
 if __name__ == '__main__':
+
     u0 = np.array([0, 0, 0, 10, 0, 0], dtype=float)
-    ts = np.linspace(0, 10, 10000)
+    ts = np.linspace(0, duration, duration/dt)
     us = odeint(ddt, u0, ts)
-
-    labels = ['x', 'y', 'theta', 'xdot', 'ydot', 'thetadot']
-
-
-    #for label, data in zip(labels, us.T):
-    #    plt.plot(ts, data, label=label)
 
     x, y, theta, xdot, ydot, thetadot = us.T
     phi = np.arctan2(-xdot, ydot) # the of the velocity vector away from vertical
     animate(x, y, theta)
 
-    #plt.plot(ts, phi, label='phi')
-    #plt.legend()
-    #plt.show()
+    # labels = ['x', 'y', 'theta', 'xdot', 'ydot', 'thetadot']
+    # for label, data in zip(labels, us.T):
+    #     plt.plot(ts, data, label=label)
+
+    # plt.plot(ts, phi, label='phi')
+    # plt.legend()
+    # plt.show()
