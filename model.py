7f28b1d9fb370684a6bb52bbc51e1a9e0bc97bd8
Izzy Brand <isaiah_brand@brown.edu>
2019-05-03 11:12:48 -0400

    added control over thrust and amde the rocket do the spacex thing (was wayy too easy)

--- model.py
+++ model.py
@@ -10,29 +10,41 @@ cos = np.cos
 # state is [x, y, theta, xdot, ydot, thetadot]
 
 # dynamics parameters
-T = 0.        # thrust
+# T = 11       # thrust
 L = -1.        # length from CG of rocket to the motor
-Dv = 0.1        # the linear drag on the rocket
-Dw = 1.     # the rotational drag on the rocket
+Dv = 0.        # the linear drag on the rocket
+Dw = 0.     # the rotational drag on the rocket
 I = 1.        # the rotational inertia of the rocket
-l = -0.5        # the distance from the CG of the rocket to the center of aerodynamic pressure
-g = 9.8     # acceleration due to gravity
+l = -0        # the distance from the CG of the rocket to the center of aerodynamic pressure
+g = 10     # acceleration due to gravity
 m = 1
 
 # simulation/rendering parameters
 dt = 0.001
-duration = 5
+duration = 10
 framerate = 20
 rocket_size = 1
 
 # a function that returns a thrust angle based on the rocket's state
 def control(u):
-    return 0
+    x, y, theta, xdot, ydot, thetadot = u
+
+    angle_control = theta + thetadot 
+    x_control = - 0.05*xdot - 0.01*x
+    return angle_control + x_control
+
+    # max_angle = np.pi/4
+    # return np.clip(10*theta, -max_angle, max_angle)
+
+def thrust(u):
+    x, y, theta, xdot, ydot, thetadot = u
+    return np.clip(g - 0.1*y - ydot, 0, 50)
 
 # the rocket dynamics
 def ddt(u, t):
     x, y, theta, xdot, ydot, thetadot = u
 
+    T = thrust(u)
     c = control(u)                  # the control input
     v2 = (xdot**2 + ydot**2)      # the speed squared
     phi = np.arctan2(-xdot, ydot) # the of the velocity vector away from vertical
@@ -74,7 +86,14 @@ def animate(x, y, theta):
 
 if __name__ == '__main__':
 
-    u0 = np.array([0, 0, 0.05, 0, 0, 0], dtype=float)
+    x           = 20
+    y           = 50
+    theta       = 0
+    x_dot       = 0
+    y_dot       = 0
+    theta_dot   = 0
+
+    u0 = np.array([x, y, theta, x_dot, y_dot, theta_dot], dtype=float)
     ts = np.linspace(0, duration, duration/dt)
     us = odeint(ddt, u0, ts)
 
